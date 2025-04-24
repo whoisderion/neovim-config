49b1a351bac718eda57e47c83abd9fff88f88b08
Derion Williams <derionwllms@gmail.com>
2025-04-24 11:09:37 -0500

    Updated config for Windows 11 compatibility

--- init.lua
+++ init.lua
@@ -4,3 +4,17 @@ require("derion.core.colorscheme")
 require("derion.lazy")
 --require("mason").setup()
 --require("mason-lspconfig").setup()
+--
+-- For Windows clipboard support
+vim.g.clipboard = {
+	name = "win32yank-wsl",
+	copy = {
+		["+"] = "win32yank.exe -i --crlf",
+		["*"] = "win32yank.exe -i --crlf",
+	},
+	paste = {
+		["+"] = "win32yank.exe -o --lf",
+		["*"] = "win32yank.exe -o --lf",
+	},
+	cache_enabled = 0,
+}
